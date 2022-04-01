cc15cf430def7b9c394ebe1cf49014741a656baf
SaloniKhaparde <101316721+SaloniKhaparde@users.noreply.github.com>
2022-04-01 17:21:27 +0530

    Create project3.c

---
+++ 3_Implementation/project3.c
@@ -0,0 +1,37 @@
+void go(int n)
+{
+    if(turn % 2)
+        board[n] = 3;
+    else
+        board[n] = 5;
+    turn++;
+}
+
+void player_first()
+{
+    int pos;
+
+    check_draw();
+    draw_board();
+    gotoxy(30,18);
+    printf("Your Turn :> ");
+    scanf("%d",&pos);
+
+    if(board[pos] != 2)
+        player_first();
+
+    if(pos == posswin(player))
+    {
+        go(pos);
+        draw_board();
+        gotoxy(30,20);
+        //textcolor(128+RED);
+        printf("Player Wins");
+        getch();
+        exit(0);
+    }
+
+    go(pos);
+    draw_board();
+    start_game();
+}
